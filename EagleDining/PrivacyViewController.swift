a4ffa933d59a2d618fc303d41ed761e7c6014719
Jack Antcio <anticoj@bc.edu>
2020-03-16 22:31:18 -0400

    Added PrivacyViewController

---
+++ EagleDining/PrivacyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PrivacyViewController.swift
+//  EagleDining
+//
+//  Created by Jack Antico on 3/16/20.
+//  Copyright © 2020 Jack Antico. All rights reserved.
+//
+
+import UIKit
+
+class PrivacyViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
